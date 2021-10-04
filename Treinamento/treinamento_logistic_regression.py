41d23f84d7329e6cbe4840fcfd3ed831522a005c
LBBassani <lorenabassani12@gmail.com>
2021-10-04 17:54:34 -0300

    Ajeitando base de dados para treinamento

---
+++ Treinamento/treinamento_logistic_regression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#%%
+import pandas as pd
+
+#%%
+rssi_values = {"notebook-hotspot" : list(),
+               "@2830" : list(),
+               "@2830_Ext" : list(),
+               "position" : list()}
+
+#%%
+rssi_values["notebook-hotspot"].extend([-42,-45,-44,-78,-79,-76])
+rssi_values["@2830"].extend([-78,-79,-76,-78,-79,-76])
+rssi_values["@2830_Ext"].extend([-78,-79,-76,-42,-45,-44])
+rssi_values["position"].extend([1,1,1,5,5,5])
+
+df = pd.DataFrame(data=rssi_values)
+
+#%%
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MultiLabelBinarizer
+
+#%%
+# generate 2d classification dataset
+X = pd.DataFrame(data = df, columns=['notebook-hotspot', '@2830', '@2830_Ext'])
+y = pd.DataFrame(data = df, columns=['position'])
+
+#%%
+y = MultiLabelBinarizer().fit_transform(y)
+
+#%%
+# fit final model
+model = LogisticRegression()
+model.fit(X, y.values.ravel())
+
+#%%
+ynew = model.predict_proba(X)
