2ede36a7ee193691572191b3fc587e5a66098cb6
LBBassani <lorenabassani12@gmail.com>
2021-10-04 17:58:40 -0300

    Referencias

--- Treinamento/treinamento_logistic_regression.py
+++ Treinamento/treinamento_logistic_regression.py
@@ -14,9 +14,10 @@ rssi_values["@2830"].extend([-78,-79,-76,-78,-79,-76])
 rssi_values["@2830_Ext"].extend([-78,-79,-76,-42,-45,-44])
 rssi_values["position"].extend([1,1,1,5,5,5])
 
-df = pd.DataFrame(data=rssi_values)
+df = pd.DataFrame(data=rssi_values) # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
 
 #%%
+# https://machinelearningmastery.com/make-predictions-scikit-learn/
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MultiLabelBinarizer
 
@@ -31,7 +32,7 @@ y = MultiLabelBinarizer().fit_transform(y)
 #%%
 # fit final model
 model = LogisticRegression()
-model.fit(X, y.values.ravel())
+model.fit(X, y.values.ravel()) # https://stackoverflow.com/questions/34165731/a-column-vector-y-was-passed-when-a-1d-array-was-expected
 
 #%%
 ynew = model.predict_proba(X)
